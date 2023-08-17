0d6740de186a0edc0da21211b35f21eb5d21ae2a
Felipe de Souza <113396587+felipesouzafer@users.noreply.github.com>
2023-08-17 08:38:35 -0300

    Add files via upload

---
+++ exercicio-C/exercise03.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int idade_carro;
+	printf("Digite a idade de seu carro \n");
+	scanf("%d", &idade_carro);
+	if(idade_carro <= 3){
+		printf("Seu carro é novo \n");
+	}else{
+		printf("Seu carro é velho \n");
+}
+}
