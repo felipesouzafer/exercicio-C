98937543bad89f9276abfce8aa9d313a94fb754d
Felipe de Souza <113396587+felipesouzafer@users.noreply.github.com>
2023-08-17 09:04:56 -0300

    exercício 05 - estruturas aninhadas

---
+++ exercicio-C/exercise05.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+	int categoria; float preco;
+	scanf("%d", &categoria);
+	if (categoria == 1){
+		preco = 10;
+	}else{
+			if (categoria == 2){
+		preco = 18;
+	}else{
+	 	if (categoria == 3){
+		preco = 23;
+} else {
+	printf("Categoria inexistente \n");
+		preco = 0;
+}
+}
+		
+	}
+	
+
